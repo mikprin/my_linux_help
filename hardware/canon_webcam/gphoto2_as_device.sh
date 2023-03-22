800a637e0119ffd1e0a420f188fc360768843fc1
Mikhail Solovyanov <mikhail.solovyanov@bostongene.com>
2023-03-22 12:32:12 +0400

    hardware/canon_webcam/gphoto2_as_device.sh

--- hardware/canon_webcam/gphoto2_as_device.sh
+++ hardware/canon_webcam/gphoto2_as_device.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sudo modprobe v4l2loopback
 gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video2
+# gphoto2 --stdout --set-config liveviewsize=0 --capture-movie | ffmpeg  -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 -s:v 1920x1080 -r 25 /dev/video0
